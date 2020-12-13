f30aeace4c143413e0ec5ef52a79565547271688
Diego Martinez <diegomtzg96@gmail.com>
2020-12-12 21:32:03 -0600

    Incredible nice looking navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,54 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import React from "react";
 
 function Navbar() {
-  const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <AppBar id="header" position="fixed" color="transparent">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Diego Martinez
-          </Typography>
-          <Typography>
-            About
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <nav className="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="header">
+        <div className="container">
+        <a className="navbar-brand js-scroll" href="#home">
+            <h3>Diego Martinez</h3>
+          </a>
+          <button
+            className="navbar-toggler collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarDefault"
+            aria-controls="navbarDefault"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <div
+            className="navbar-collapse collapse justify-content-end"
+            id="navbarDefault"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link js-scroll active" href="#home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll" href="#about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll" href="#work">
+                  Work
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll" href="#contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
   );
 }
 
--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,12 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: purple[500],
+      main: '#ffffff',
     },
     secondary: {
-      main: '#f44336',
+      main: '#ffffff',
     },
   },
 });

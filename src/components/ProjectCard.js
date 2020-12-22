cee703b5e84f7262872fc7f01f9bc1e57e87e0d9
Diego Martinez <diegomtzg96@gmail.com>
2020-12-22 15:25:00 -0600

    Back to repo cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,47 +1,68 @@
 import React from "react";
-import cx from "clsx";
-import GoogleFontLoader from "react-google-font-loader";
-import NoSsr from "@material-ui/core/NoSsr";
-import Avatar from "@material-ui/core/Avatar";
-import Box from "@material-ui/core/Box";
-import CardMedia from "@material-ui/core/CardMedia";
-import Grid from "@material-ui/core/Grid";
-import { useCoverCardMediaStyles } from "@mui-treasury/styles/cardMedia/cover";
-import { Row, Item } from "@mui-treasury/components/flex";
+import { Fade } from "react-reveal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function CustomCard({
-  styles,
-  cover,
-  logo,
-  title,
-  brand,
-  date,
-  projectInfo,
-}) {
-  const mediaStyles = useCoverCardMediaStyles();
+export default function ProjectCard({ projectInfo }) {
   return (
-    <Box className={cx(styles.root, styles.color)} pt={20}>
-      <CardMedia image={cover} className={styles.cover} classes={mediaStyles} />
-      <Box className={styles.content} p={2}>
-        <Box position={"relative"} zIndex={1}>
-          <Row p={0} gap={2}>
-            <Item>
-              <Avatar className={styles.logo} src={logo} />
-            </Item>
-            <Item position={"bottom"}>
-              <h2 className={styles.title}>{title}</h2>
-            </Item>
-          </Row>
-          <Row mt={4} alignItems={"center"}>
-            <Item>
-              <div className={styles.team}>{brand}</div>
-            </Item>
-            <Item position={"right"}>
-              <div className={styles.date}>{date}</div>
-            </Item>
-          </Row>
-        </Box>
-      </Box>
-    </Box>
+    <div>
+      <div className="repo-card-div">
+        <div className="project-name-div">
+          <FontAwesomeIcon icon={projectInfo.icon} size="lg" />
+          <p className="project-name">{projectInfo.name}</p>
+        </div>
+        <p className="repo-description">{projectInfo.desc}</p>
+        <div className="repo-stats">
+          <div className="repo-left-stat">
+            <span>
+              <div
+                className="language-color"
+                style={{ backgroundColor: "yellow" }}
+              ></div>
+              <p>lang</p>
+            </span>
+
+            <span>
+              <svg
+                aria-hidden="true"
+                className="octicon"
+                height="20"
+                role="img"
+                viewBox="0 0 10 16"
+                width="12"
+                fill="rgb(106, 115, 125)"
+                className="repo-star-svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"
+                ></path>
+              </svg>
+              <p>forks</p>
+            </span>
+            <span>
+              <svg
+                aria-hidden="true"
+                className="octicon"
+                height="20"
+                role="img"
+                viewBox="0 0 14 16"
+                width="14"
+                fill="rgb(106, 115, 125)"
+                className="repo-star-svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
+                ></path>
+              </svg>
+              <p>stargazers</p>
+            </span>
+          </div>
+          <div className="repo-right-stat">
+            <p>diskUsage KB</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,185 +14,69 @@ import {
 import "../static/css/Projects.css";
 import ProjectCard from "./ProjectCard";
 
-import cx from "clsx";
-import Color from "color";
-import GoogleFontLoader from "react-google-font-loader";
-import NoSsr from "@material-ui/core/NoSsr";
-import { makeStyles } from "@material-ui/core/styles";
-import Avatar from "@material-ui/core/Avatar";
-import Box from "@material-ui/core/Box";
-import CardMedia from "@material-ui/core/CardMedia";
-import { useCoverCardMediaStyles } from "@mui-treasury/styles/cardMedia/cover";
-import { Row, Item } from "@mui-treasury/components/flex";
-import vitaeImg from "../assets/images/vitae.png";
-
-const useGridStyles = makeStyles(({ breakpoints }) => ({
-  root: {
-    overflow: "auto",
-    [breakpoints.only("xs")]: {
-      "& > *:not(:first-child)": {
-        paddingLeft: 0,
-      },
-    },
-    [breakpoints.up("sm")]: {
-      justifyContent: "center",
-    },
-  },
-}));
-
-const useStyles = makeStyles(({ palette }) => ({
-  color: ({ color }) => ({
-    "&:before": {
-      backgroundColor: Color(color).darken(0.3).desaturate(0.2).toString(),
-    },
-  }),
-  root: {
-    position: "relative",
-    borderRadius: "1rem",
-    minWidth: 320,
-    "&:before": {
-      transition: "0.2s",
-      position: "absolute",
-      width: "100%",
-      height: "100%",
-      content: '""',
-      display: "block",
-      borderRadius: "1rem",
-      zIndex: 0,
-      bottom: 0,
-    },
-    "&:hover": {
-      "&:before": {
-        bottom: -6,
-      },
-      "& $logo": {
-        transform: "scale(1.1)",
-        boxShadow: "0 6px 20px 0 rgba(0,0,0,0.38)",
-      },
-    },
-  },
-  cover: {
-    borderRadius: "1rem",
-  },
-  content: ({ color }) => ({
-    position: "relative",
-    zIndex: 1,
-    borderRadius: "1rem",
-    boxShadow: `0 6px 16px 0 ${Color(color).fade(0.5)}`,
-    "&:before": {
-      content: '""',
-      display: "block",
-      position: "absolute",
-      left: 0,
-      top: 0,
-      zIndex: 0,
-      width: "100%",
-      height: "100%",
-      clipPath:
-        "polygon(0% 100%, 0% 35%, 0.3% 33%, 1% 31%, 1.5% 30%, 2% 29%, 2.5% 28.4%, 3% 27.9%, 3.3% 27.6%, 5% 27%,95% 0%,100% 0%, 100% 100%)",
-      borderRadius: "1rem",
-      background: `linear-gradient(to top, ${color}, ${Color(color)
-        .rotate(24)
-        .lighten(0.12)})`,
-    },
-  }),
-  title: {
-    fontFamily: "Fjalla One",
-    fontSize: "1.25rem",
-    color: "#fff",
-    margin: 0,
-  },
-  logo: {
-    transition: "0.3s",
-    width: 100,
-    height: 100,
-    boxShadow: "0 4px 12px 0 rgba(0,0,0,0.24)",
-    borderRadius: "1rem",
-  },
-  team: {
-    fontFamily: "Sen",
-    fontSize: "0.75rem",
-    color: palette.text.hint,
-  },
-  date: {
-    fontFamily: "Sen",
-    color: "#fff",
-    backgroundColor: palette.text.hint,
-    opacity: 0.72,
-    fontSize: "0.75rem",
-    padding: "0 0.5rem",
-    borderRadius: 12,
-  },
-}));
-
 export default function Projects() {
   return (
     <div className="section projects">
-      <div className="projects-grid">
-        <GoogleFontLoader
-          fonts={[{ font: "Fjalla One" }, { font: "Sen", weights: [500] }]}
-        />
-        <Grid
-          style={{ padding: 16 }}
-          classes={useGridStyles()}
-          wrap={"nowrap"}
-          container
-          spacing={4}
-        >
+      <Grid container alignItems="space-evenly" spacing={3}>
+        <Grid container item justify="space-evenly">
           <Grid item>
             <ProjectCard
-              styles={useStyles({ color: "#fc7944" })}
-              brand={"Overwatch Official"}
-              date={"02.04.2020"}
-              cover={vitaeImg}
-              logo={"https://d3fa68hw0m2vcc.cloudfront.net/bf4/156511609.jpeg"}
-              title={
-                <>
-                  Astronomy Binoculars
-                  <br />A Great Alternative
-                </>
-              }
+              projectInfo={{
+                icon: faCamera,
+                name: "InFrame",
+                desc: "Description",
+              }}
             />
           </Grid>
           <Grid item>
             <ProjectCard
-              styles={useStyles({ color: "#5357ff" })}
-              // "#5357ce"
-              brand={"League of Legends Official"}
-              date={"02.04.2020"}
-              cover={
-                "https://www.pcclean.io/wp-content/uploads/2019/04/559308.jpg"
-              }
-              logo={
-                "https://vignette.wikia.nocookie.net/youtube/images/7/77/LeagueOfLegends.jpg/revision/latest?cb=20180718040905"
-              }
-              title={
-                <>
-                  Astronomy Binoculars
-                  <br />A Great Alternative
-                </>
-              }
+              projectInfo={{
+                icon: faFileAlt,
+                name: "Vitae",
+                desc: "Description",
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <ProjectCard
+              projectInfo={{
+                icon: faDiceD20,
+                name: "Scotty3D",
+                desc: "Description",
+              }}
             />
           </Grid>
         </Grid>
-      </div>
+        <Grid container item justify="space-evenly">
+          <Grid item>
+            <ProjectCard
+              projectInfo={{
+                icon: faTabletAlt,
+                name: "MirrorX",
+                desc: "Description",
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <ProjectCard
+              projectInfo={{
+                icon: faLaptopCode,
+                name: "ScreenBacklight",
+                desc: "Description",
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <ProjectCard
+              projectInfo={{
+                icon: faHeartbeat,
+                name: "CDC WonderPy",
+                desc: "Description",
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
-//   return (
-//     <div className="section projects">
-//       <div className="projects-grid">
-//         <ProjectCard
-//           projectInfo={{
-//             name: "InFrame",
-//             link: "https://github.com/diegomtzg/InFrame",
-//             icon: faCamera,
-//             desc:
-//               "Robotic camera that uses real-time object detection to track user-selected targets.",
-//             descBullets: ["Bullet 1"],
-//           }}
-//         />
-//       </div>
-//     </div>
-//   );
-// }

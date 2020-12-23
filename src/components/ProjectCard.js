495d40fa5d50b0fdcc09af4a0c09e396346fa885
Diego Martinez <diegomtzg96@gmail.com>
2020-12-22 21:12:37 -0600

    Adds skills tags to projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,7 +1,50 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEye,
+  faRobot,
+  faLaptopCode,
+  faHeartbeat,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function ProjectCard({ projectInfo }) {
+  const GetSkills = ({ skills }) => {
+    if (skills) {
+      return skills.map((skill) => {
+        var icon, color;
+        switch (skill) {
+          case "Computer Vision":
+            icon = faEye;
+            color = "#00e1ff";
+            break;
+          case "Robotics":
+            icon = faRobot;
+            color = "red";
+            break;
+          case "Web Dev":
+            icon = faLaptopCode;
+            color = "purple";
+            break;
+          default:
+            icon = faHeartbeat;
+            color = "gray";
+            break;
+        }
+        return (
+          <div className="project-left-tag">
+            <FontAwesomeIcon
+              className="project-left-icon"
+              icon={icon}
+              color={color}
+              size="lg"
+            />
+            {skill}
+          </div>
+        );
+      });
+    } else return null;
+  };
+
   return (
     <a href={projectInfo.link} target="_blank" rel="noopener noreferrer">
       <div className="project-card-div">
@@ -18,17 +61,9 @@ export default function ProjectCard({ projectInfo }) {
         <p className="project-description">{projectInfo.desc}</p>
         <div className="project-stats">
           <div className="project-left-stat">
-            <span className="project-left-tag">
-              <FontAwesomeIcon
-                className="project-left-icon"
-                color={projectInfo.skillColor}
-                icon={projectInfo.skillIcon}
-                size="lg"
-              />
-              Skill
-            </span>
+            <GetSkills skills={projectInfo.skills} />
           </div>
-          <div className="project-right-tag">diskUsage KB</div>
+          <div className="project-right-tag">{projectInfo.date}</div>
         </div>
       </div>
     </a>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,11 +26,9 @@ export default function Projects() {
             name: "InFrame",
             link: "https://github.com/diegomtzg/InFrame",
             icon: faCamera,
-            skillIcon: faCamera,
-            skillColor: "red",
             desc:
               "Robotic camera that uses real-time object detection to track user-selected targets.",
-            descBullets: ["Bullet 1"],
+            skills: ["Computer Vision", "Robotics"],
           }}
         />
 
@@ -41,7 +39,7 @@ export default function Projects() {
             icon: faFileAlt,
             desc:
               "Full stack web-application that lets users easily create beautiful online portfolios to showcase their professional experiences.",
-            descBullets: ["Bullet 1"],
+            skills: ["Web Dev"],
           }}
         />
 
@@ -52,29 +50,7 @@ export default function Projects() {
             icon: faDiceD20,
             desc:
               "3D graphics software built from scratch: Features include: Interactive mesh editing, realistic path tracing and dynamic animation.",
-            descBullets: ["Bullet 1"],
-          }}
-        />
-
-        <ProjectCard
-          projectInfo={{
-            name: "MirrorX",
-            link: "https://github.com/diegomtzg/MirrorX",
-            icon: faTabletAlt,
-            desc:
-              "Smart mirror with facial recognition login that pulls daily info from various public APIs.",
-            descBullets: ["Bullet 1"],
-          }}
-        />
-
-        <ProjectCard
-          projectInfo={{
-            name: "ScreenBacklight",
-            link: "https://github.com/diegomtzg/ScreenBacklight",
-            icon: faLaptopCode,
-            desc:
-              "Ambient lighting system to enhance movie viewing and music listening experience.",
-            descBullets: ["Bullet 1"],
+            skills: ["Computer Vision", "Somethin"],
           }}
         />
 
@@ -85,7 +61,29 @@ export default function Projects() {
             icon: faHeartbeat,
             desc:
               "An API wrapper designed to improve access to the CDC's WONDER database.",
-            descBullets: ["Bullet 1"],
+            skills: ["Computer Vision", "Somethin"],
+          }}
+        />
+
+        <ProjectCard
+          projectInfo={{
+            name: "MirrorX",
+            link: "https://github.com/diegomtzg/MirrorX",
+            icon: faTabletAlt,
+            desc:
+              "Smart mirror with facial recognition login that pulls daily info from various public APIs.",
+            skills: ["Computer Vision", "Somethin"],
+          }}
+        />
+
+        <ProjectCard
+          projectInfo={{
+            name: "ScreenBacklight",
+            link: "https://github.com/diegomtzg/ScreenBacklight",
+            icon: faLaptopCode,
+            desc:
+              "Ambient lighting system to enhance movie viewing and music listening experience.",
+            skills: ["Computer Vision", "Somethin"],
           }}
         />
       </div>
